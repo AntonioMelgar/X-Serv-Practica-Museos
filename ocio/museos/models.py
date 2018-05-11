e8d38b07944b53c8a1f761a608ff04f69538b24a
AntonioMelgar <a.melgarg77@gmail.com>
2018-05-11 22:09:35 +0200

    Procesado de los datos en formato XML y guardado en la DataBase

--- ocio/museos/models.py
+++ ocio/museos/models.py
@@ -2,17 +2,33 @@ from django.db import models
 
 # Create your models here.
 class Museo(models.Model):
-	p_k = models.IntegerField()	
-	nombre = models.CharField(max_length=256)
-	horario = models.CharField(max_length=20000)
-	descripcion = models.CharField(max_length=20000)
-	direccion = models.CharField(max_length=20000)
-	distrito = models.CharField(max_length=256)
-	accesibilidad = models.IntegerField()
-	enlace = models.CharField(max_length=20000)
-	numero_comentarios = models.IntegerField()
+	ID_ENTIDAD = models.CharField(max_length=256)	
+	NOMBRE = models.CharField(max_length=256)
+	DESCRIPCION_ENTIDAD = models.CharField(max_length=20000)
+	HORARIO = models.CharField(max_length=20000)
+	TRANSPORTE = models.CharField(max_length=20000)
+	ACCESIBILIDAD = models.CharField(max_length=256)
+	CONTENT_URL = models.CharField(max_length=20000)
+	NOMBRE_VIA = models.CharField(max_length=20000)
+	CLASE_VIAL =  models.CharField(max_length=20000)
+	TIPO_NUM =  models.CharField(max_length=20000)
+	NUM = models.CharField(max_length=20000)
+	LOCALIDAD = models.CharField(max_length=20000)
+	CODIGO_POSTAL = models.CharField(max_length=20000)
+	PLANTA = models.CharField(max_length=20000)
+	BARRIO = models.CharField(max_length=20000)
+	DISTRITO =  models.CharField(max_length=20000)
+	COORDENADA_X = models.CharField(max_length=20000)
+	COORDENADA_Y = models.CharField(max_length=20000)
+	LATITUD = models.CharField(max_length=20000)
+	LONGITUD = models.CharField(max_length=20000)
+	TELEFONO =  models.CharField(max_length=20000)
+	FAX =  models.CharField(max_length=20000)
+	EMAIL =  models.CharField(max_length=20000)
+	EQUIPAMIENTO =  models.CharField(max_length=20000)
+	NUMERO_COMENTARIOS = models.IntegerField()
 	def __str__(self):
-		return self.nombre	
+		return self.NOMBRE	
 
 class Usuario(models.Model):
 	nombre = models.CharField(max_length=256)
@@ -20,6 +36,6 @@ class Usuario(models.Model):
 	fecha = models.DateTimeField() 
 	museo = models.ForeignKey(Museo)
 	def __str__(self):
-		return self.museo.nombre + ', ' + self.comentario	
+		return self.museo.NOMBRE + ', ' + self.comentario	
 
 
--- ocio/museos/views.py
+++ ocio/museos/views.py
@@ -1,9 +1,79 @@
+#!/usr/bin/python3
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Museo, Usuario
+from lxml import etree
+
  
 # Create your views here.
+
+def extraer_elemento(dic, elemento):
+	
+	try:
+		elemento = dic[elemento]
+	
+	except KeyError:
+		elemento = ""
+
+	return elemento
+
+	
+
+def guardar_datos(dic):
+	dicc_datos = {}
+	lista_aux = ['ID_ENTIDAD', 'NOMBRE', 'DESCRIPCION_ENTIDAD', 'HORARIO', 'TRANSPORTE', 'ACCESIBILIDAD', 'CONTENT_URL', 'NOMBRE_VIA', 'CLASE_VIAL', 'TIPO_NUM', 'NUM', 'LOCALIDAD', 'CODIGO_POSTAL','PLANTA', 'BARRIO', 'DISTRITO', 'COORDENADA_X', 'COORDENADA_Y', 'LATITUD', 'LONGITUD', 'TELEFONO', 'FAX', 'EMAIL', 'EQUIPAMIENTO']
+	
+	for elemento in lista_aux:
+		dicc_datos[elemento] = extraer_elemento(dic, elemento)
+
+	g = Museo(ID_ENTIDAD = dicc_datos['ID_ENTIDAD'], NOMBRE = dicc_datos['NOMBRE'], DESCRIPCION_ENTIDAD = dicc_datos['DESCRIPCION_ENTIDAD'], HORARIO = dicc_datos['HORARIO'], TRANSPORTE = dicc_datos['TRANSPORTE'], ACCESIBILIDAD = dicc_datos['ACCESIBILIDAD'], CONTENT_URL = dicc_datos['CONTENT_URL'], NOMBRE_VIA = dicc_datos['NOMBRE_VIA'], CLASE_VIAL = dicc_datos['CLASE_VIAL'], TIPO_NUM = dicc_datos['TIPO_NUM'], NUM = dicc_datos['NUM'], LOCALIDAD = dicc_datos['LOCALIDAD'], CODIGO_POSTAL = dicc_datos['CODIGO_POSTAL'], PLANTA = dicc_datos['PLANTA'], BARRIO = dicc_datos['BARRIO'], DISTRITO = dicc_datos['DISTRITO'], COORDENADA_X = dicc_datos['COORDENADA_X'], COORDENADA_Y = dicc_datos['COORDENADA_Y'], LATITUD = dicc_datos['LATITUD'], LONGITUD = dicc_datos['LONGITUD'], TELEFONO = dicc_datos['TELEFONO'], FAX = dicc_datos['FAX'], EMAIL = dicc_datos['EMAIL'], EQUIPAMIENTO = dicc_datos['EQUIPAMIENTO'], NUMERO_COMENTARIOS = 0)
+	
+	g.save()
+		
+	return None
+	
+
+def parser():
+	
+	doc = etree.parse('museos/201132-0-museos.xml')
+	contenidos = doc.getroot()
+	
+	for k in range(1, len(contenidos)):
+		contenido = contenidos[k]
+		atributo = contenido[1]			
+		dic = {}
+		
+		for i in range(0, len(atributo)-1):
+			nombre = atributo[i].attrib.get("nombre") 
+			if nombre == "LOCALIZACION":
+				for j in range(0, len(atributo[i])):				
+					nombre = atributo[i][j].attrib.get("nombre")
+					val = nombre.find("-")
+					if val != -1:
+						nombre = nombre.replace("-", "_")
+
+					dic[nombre] = atributo[i][j].text
+	 		
+			elif nombre == "DATOSCONTACTOS":
+				for j in range(0, len(atributo[i])):
+					nombre = atributo[i][j].attrib.get("nombre")
+					val = nombre.find("-")
+					if val != -1:
+						nombre = nombre.replace("-", "_")
+
+					dic[nombre] = atributo[i][j].text
+			else:
+				val = nombre.find("-")
+				if val != -1:
+					nombre = nombre.replace("-", "_")
+
+				dic[nombre] = atributo[i].text
+
+		guardar_datos(dic) 
+		
+	return None
+		
 	
 @csrf_exempt
 def mostrar_principal(request):
@@ -16,7 +86,7 @@ def mostrar_principal(request):
 		name_link = 'Login'
 		link = 'login'		
 
-	lista_museos = Museo.objects.order_by('-numero_comentarios')[0:5]
+	lista_museos = Museo.objects.order_by('-NUMERO_COMENTARIOS')[0:5]
 	if request.method == "GET":
 		respuesta = render(request, 'museos/index.html', {'lista_museos': lista_museos, 'logged': logged, 'link': link, 'name_link': name_link})
 	
@@ -24,7 +94,10 @@ def mostrar_principal(request):
 		if 'Todos' in request.POST:
    			respuesta = HttpResponseRedirect('/museos')
 		elif 'About' in request.POST:
-			respuesta = HttpResponseRedirect('/about')	
+			respuesta = HttpResponseRedirect('/about')		
+		elif 'Cargar' in request.POST: ######
+			parser()	
+			respuesta = HttpResponseRedirect('/')
 
 	return respuesta
 
@@ -35,7 +108,7 @@ def mostrar_museos(request):
 		respuesta = render(request, 'museos/museos.html', {'lista_museos': lista_museos})
 	elif request.method == "POST":
 		distrito = request.POST['Distrito']
-		lista_museos = Museo.objects.filter(distrito=distrito)
+		lista_museos = Museo.objects.filter(DISTRITO=distrito)
 		respuesta = render(request, 'museos/museos.html', {'lista_museos': lista_museos})
 
 	return respuesta 
